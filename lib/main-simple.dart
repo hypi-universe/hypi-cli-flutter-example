cbfbf566dd3c84cd5315445b5efd5504cb09bfd7
Eman <eman.cse2008@gmail.com>
2021-04-07 00:57:46 +0200

    change schema.graphql to generated-schema.graphql

--- lib/main-simple.dart
+++ lib/main-simple.dart
@@ -9,8 +9,8 @@ class AuthenticatedClient extends http.BaseClient {
 
   Future<http.StreamedResponse> send(http.BaseRequest request) {
     request.headers['Authorization'] =
-        'Bearer eyJhbGciOiJSUzI1NiJ9.eyJoeXBpLmluc3RhbmNlIjp7InJlYWxtIjoiZW1hbiIsIm5hbWUiOiJzdG9yZSIsInJlbGVhc2UiOiJsYXRlc3QifSwiaHlwaS5sb2dpbiI6dHJ1ZSwiaHlwaS5wdXJwb3NlIjoibG9naW4iLCJoeXBpLnVzZXJuYW1lIjoiZW1hbi5jc2UyMDA4QGdtYWlsLmNvbSIsImh5cGkuZW1haWwiOiJlbWFuLmNzZTIwMDhAZ21haWwuY29tIiwiYXVkIjoiZW1hbiIsImlhdCI6MTYxNDYyMDI1MCwiZXhwIjoxNjE3MjEyMjUwLCJzdWIiOiIwMUVOM1NCSkFDMzJBTkdQQjhKUVBQMFlTNCIsIm5iZiI6MTYxNDYyMDI1MH0.UOLOJyb_edS9b9tNQUekVDMA9mXvu9_6vY1Lfnprv4mS9YemmWl_Hp9vi1jAw-JAhI88VRwuVQO_Oj2cxbY1X8FPCQZEK7EdVupNjr4ReC3z8zeM3EUVOXzwIlJcNQ1GgWljVpVaQsObPl8EAVN9PAGShTn8WC1bC1aWyOqRgwraj9cDJNdf8c2EYrJFvcnkHf2SNl9FAPgPDj3pX3d65UJX6EPE9ptdwXgQm78_IXY8HGdYN6n86i57579pTVycDi24iXGn62kOskRurlne5yL2ecrPuYl-9gVlGxZALpkh8aSevwyQGBxnIUJ2BZBoDqsFkBcq-qcxKiCW0aObgw';
-    request.headers['hypi-domain'] = 'conveyors.apps.hypi.app';
+        'Bearer eyJhbGciOiJSUzI1NiJ9.eyJoeXBpLmxvZ2luIjp0cnVlLCJoeXBpLnVzZXJuYW1lIjoiZW1hbi5jc2UyMDA4QGdtYWlsLmNvbSIsImh5cGkuZW1haWwiOiJlbWFuLmNzZTIwMDhAZ21haWwuY29tIiwiYXVkIjoiMDFGMkdaQkpLSDZSM1RDNkVKRFJFNU5IRzQiLCJpYXQiOjE2MTc3MjQ3MDUsImV4cCI6MTYyMDMxNjcwNSwic3ViIjoiMDFGMkdaQkpLQUgxSkNDQlZaUzI0TVQ3VlIiLCJuYmYiOjE2MTc3MjQ3MDV9.PZALeFiFCFisNf1ll6AIDj387WAen3xzgcTS3UBe2J3F3WNS05jcPXqQoCk6LEJyJp2wJr1sC5v9OutDUVxm9aaVTmIvEUrdR7YBfZauytcOY7xWzMvH3kbp6zUElNh7OjLbxu8KVzPKAnjdarSZPP0fwpdonb0Mh_yISyVRDWGckw2qwH8BmbGhKN6UwM8xjVLePc0d9qDj_CQ_XroqyZHjCN0Nbf1pP0edM4C1dUEKWJrAEIjDTiBitCtQHF8bv_z53eTSPCfjdueNhwwXGUIIz2-axn2ki31k6OUmopYzkZvnBuUltRueBEALFmJK12p6q_iGVtnZ389MJWbemA';
+    request.headers['hypi-domain'] = 'my-dom.com';
     return _inner.send(request);
   }
 }
@@ -28,33 +28,10 @@ void main() async {
   if (response.hasErrors) {
     return print('Error: ${response.errors.map((e) => e.message).toList()}');
   }
-//  if (response.data.find.edges.isNotEmpty) {
-//    final products = response.data.find.edges.whereType<ProductsData$Query$Find$Edges>();
-//    print(products.first.node.props);
-//  }
+
   print(response.data);
   (response?.data?.find?.edges ?? [])
       .map((r) => r.node.title)
       .forEach(print);
   print('Hello after response print');
-//  final node = response.data.find.edges[0].node;
-//  print(node.props);
-//  runApp(MyApp());
 }
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Welcome to Flutter',
-//      home: Scaffold(
-//        appBar: AppBar(
-//          title: Text('Welcome to Flutter'),
-//        ),
-//        body: Center(
-//          child: Text('Hello World'),
-//        ),
-//      ),
-//    );
-//
-//  }
-//}
